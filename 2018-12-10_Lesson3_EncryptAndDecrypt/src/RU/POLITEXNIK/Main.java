22f72c13c04046fb3140219adc67176653fece4e
Yakov Alekseev <40690132+politexnik@users.noreply.github.com>
2018-12-11 17:10:44 +0300

    Lesson3-Алгоритм шифрования/дешифровки сдвигом
    Has done

--- 2018-12-10_Lesson3_EncryptAndDecrypt/src/RU/POLITEXNIK/Main.java
+++ 2018-12-10_Lesson3_EncryptAndDecrypt/src/RU/POLITEXNIK/Main.java
@@ -1,27 +1,76 @@
 package RU.POLITEXNIK;
 
-import java.util.Arrays;
+import java.util.*;
 
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args) throws Exception{
+	// проверка работы
+        byte[] arr = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18};
+        byte[] key = {3, 2, 5, 2, 4 };
+        System.out.println("Исходный массив\n" + Arrays.toString(arr));
+        arr = encryptArr(arr, key);
+        System.out.println("Закодированный массив\n" + Arrays.toString(arr));
+        arr = decryptArr(arr, key);
+        System.out.println("Раскодированный массив\n" + Arrays.toString(arr));
     }
 
-    public byte[] encryptArr(byte[] baseArr, byte[] key){
-       //TODO Закончить метод
+    //кодирование массива
+    public static byte[] encryptArr(byte[] baseArr, byte[] key) throws Exception{
+        if (baseArr.length / key.length < 2) {
+            throw new Exception("The key array length is too low");
+        }
+        byte[] adaptedArr = adaptArrToEncryption(baseArr, key);
 
+        int partLength = adaptedArr.length / key.length;        // длина куска разбиения
+        KeyNode[] keys = new KeyNode[key.length];       //направляющие ключа для кодировки
+        for (int i = 0; i < key.length; i++) {
+            keys[i] = new KeyNode(key[i], i * partLength);
+        }
+
+        Arrays.sort(keys, (o1, o2) -> o1.key - o2.key);
+
+        byte[] result = new byte[adaptedArr.length];
+        int count = 0;
+        for (KeyNode entry: keys) {
+            copyRange(adaptedArr, result, entry.value, count * partLength, partLength);
+            count++;
+        }
+        return result;
+    }
+
+    public static byte[] decryptArr(byte[] encryptedArr, byte[] key) throws Exception{
+        int partLength = encryptedArr.length / key.length;
+        if (partLength < 2) {
+            throw new Exception("The key array length is too low");
+        }
+        byte[] adaptedArr = new byte[encryptedArr.length];
+        KeyNode[] keys = new KeyNode[key.length];
+
+        for (int i = 0; i < key.length; i++) {
+            keys[i] = new KeyNode(key[i], i * partLength);
+        }
+
+        Arrays.sort(keys, (o1, o2) -> o1.key - o2.key);
+
+        for (int i = 0; i < key.length; i++) {
+         copyRange(encryptedArr, adaptedArr, i * partLength, keys[i].value, partLength);
+        }
+        //System.out.println(Arrays.toString(adaptedArr));
+
+        for (int i = adaptedArr.length - 1; i >= 0 ; i--) {
+            if (adaptedArr[i] == 1) {
+                return Arrays.copyOf(adaptedArr, i);
+            }
+        }
 
         return null;
     }
 
-    //метод для копирования части массива с места на место
-    public static void moveRange(byte[] arr, int fromPos, int toPos,int length) {
-        byte temp;
-        for (int i = 0; i < length - 1; i++) {
-            temp = arr[fromPos + i];
-            arr[fromPos + i] = arr[toPos + i];
-            arr[toPos + i] = temp;
+    //метод для копирования части массива
+    public static void copyRange(byte[] arrFrom, byte[] arrTo, int fromIndex, int toIndex, int length){
+        for (int i = 0; i < length; i++) {
+            arrTo[i + toIndex] = arrFrom[i + fromIndex];
         }
     }
 
@@ -31,7 +80,7 @@ public class Main {
         int keyLength = keyArr.length;
         int newLength = 0;
         if (baseLength % keyLength == 0) {
-             newLength = baseLength + (baseLength / keyLength);
+             newLength = baseLength + keyLength;
         } else {
             newLength = (baseLength / keyLength + 1) * keyLength;
         }
@@ -40,6 +89,12 @@ public class Main {
         return resultArr;
     }
 
-
-
+    public static class KeyNode {
+        byte key;
+        int value;
+        public KeyNode(byte key, int value){
+            this.key = key;
+            this.value = value;
+        }
+    }
 }
