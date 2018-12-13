f98d2c8b3e1c6926899feb7a05255e7303d95f1d
Yakov Alekseev <40690132+politexnik@users.noreply.github.com>
2018-12-13 10:48:38 +0300

    Solved
    Доделан алгоритм. Для ускорения необходимо написать свою одновременную сортировку всех кусков массивов

--- 2018-12-10_Lesson3_EncryptAndDecrypt/src/RU/POLITEXNIK/Main.java
+++ 2018-12-10_Lesson3_EncryptAndDecrypt/src/RU/POLITEXNIK/Main.java
@@ -5,9 +5,9 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) throws Exception{
-	// проверка работы
-        byte[] arr = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18};
-        byte[] key = {3, 2, 5, 2, 4 };
+        // проверка работы
+        byte[] arr = {0,1,2,3,4,5,6,7};
+        byte[] key = {2, 1, 0};
         System.out.println("Исходный массив\n" + Arrays.toString(arr));
         arr = encryptArr(arr, key);
         System.out.println("Закодированный массив\n" + Arrays.toString(arr));
@@ -21,58 +21,39 @@ public class Main {
             throw new Exception("The key array length is too low");
         }
         byte[] adaptedArr = adaptArrToEncryption(baseArr, key);
+        int partLength = key.length;        // длина куска разбиения
 
-        int partLength = adaptedArr.length / key.length;        // длина куска разбиения
-        KeyNode[] keys = new KeyNode[key.length];       //направляющие ключа для кодировки
-        for (int i = 0; i < key.length; i++) {
-            keys[i] = new KeyNode(key[i], i * partLength);
+        for (int i = 0; i < adaptedArr.length / key.length; i++) {
+            encryptPartOfArray(adaptedArr, i * partLength, key);
         }
 
-        Arrays.sort(keys, (o1, o2) -> o1.key - o2.key);
-
-        byte[] result = new byte[adaptedArr.length];
-        int count = 0;
-        for (KeyNode entry: keys) {
-            copyRange(adaptedArr, result, entry.value, count * partLength, partLength);
-            count++;
-        }
-        return result;
+        return adaptedArr;
     }
 
     public static byte[] decryptArr(byte[] encryptedArr, byte[] key) throws Exception{
-        int partLength = encryptedArr.length / key.length;
+        int partLength = key.length;
         if (partLength < 2) {
             throw new Exception("The key array length is too low");
         }
-        byte[] adaptedArr = new byte[encryptedArr.length];
-        KeyNode[] keys = new KeyNode[key.length];
 
-        for (int i = 0; i < key.length; i++) {
-            keys[i] = new KeyNode(key[i], i * partLength);
+        for (int i = 0; i < encryptedArr.length / key.length; i++) {
+            decryptPartOfArray(encryptedArr, i * partLength, key);
         }
 
-        Arrays.sort(keys, (o1, o2) -> o1.key - o2.key);
-
-        for (int i = 0; i < key.length; i++) {
-         copyRange(encryptedArr, adaptedArr, i * partLength, keys[i].value, partLength);
-        }
-        //System.out.println(Arrays.toString(adaptedArr));
-
-        for (int i = adaptedArr.length - 1; i >= 0 ; i--) {
-            if (adaptedArr[i] == 1) {
-                return Arrays.copyOf(adaptedArr, i);
+        for (int i = encryptedArr.length - 1; i >= 0 ; i--) {
+            if (encryptedArr[i] == 1) {
+                return Arrays.copyOf(encryptedArr, i);
             }
         }
-
         return null;
     }
 
     //метод для копирования части массива
-    public static void copyRange(byte[] arrFrom, byte[] arrTo, int fromIndex, int toIndex, int length){
-        for (int i = 0; i < length; i++) {
-            arrTo[i + toIndex] = arrFrom[i + fromIndex];
-        }
-    }
+//    public static void copyRange(byte[] arrFrom, byte[] arrTo, int fromIndex, int toIndex, int length){
+//        for (int i = 0; i < length; i++) {
+//            arrTo[i + toIndex] = arrFrom[i + fromIndex];
+//        }
+//    }
 
     //подготовка массива к шифрованию, докопирование до нужного размера
     public static byte[] adaptArrToEncryption(byte[] baseArr, byte[] keyArr) {
@@ -80,7 +61,7 @@ public class Main {
         int keyLength = keyArr.length;
         int newLength = 0;
         if (baseLength % keyLength == 0) {
-             newLength = baseLength + keyLength;
+            newLength = baseLength + keyLength;
         } else {
             newLength = (baseLength / keyLength + 1) * keyLength;
         }
@@ -95,6 +76,33 @@ public class Main {
         public KeyNode(byte key, int value){
             this.key = key;
             this.value = value;
+        }
+    }
+
+    public static void encryptPartOfArray(byte[] arr, int startIndex, byte[] key){
+        KeyNode[] keys = new KeyNode[key.length];
+        for (int i = 0; i < key.length; i++) {
+            keys[i] = new KeyNode(key[i], arr[startIndex + i]);
+        }
+        Arrays.sort(keys, (o1, o2) -> o1.key - o2.key);
+
+        for (int i = 0; i < key.length; i++) {
+            arr[startIndex + i] = (byte)keys[i].value;
+        }
+    }
+
+    public static void decryptPartOfArray(byte[] arr, int startIndex, byte[] key){
+        KeyNode[] keys = new KeyNode[key.length];
+        for (byte i = 0; i < key.length; i++) {
+            keys[i] = new KeyNode(key[i], i);
+        }
+        Arrays.sort(keys, (o1, o2) -> o1.key - o2.key);
+        byte[] tempArr = new byte[key.length];
+        for (int i = 0; i < key.length; i++) {
+            tempArr[i] = arr[keys[i].value + startIndex];
+        }
+        for (int i = 0; i < key.length; i++) {
+            arr[startIndex + i] = tempArr[i];
         }
     }
 }
